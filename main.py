2ddb8cdbc7163927a165e981f9958a17a5aa0b82
Mathew877 <mbrevans@gmail.com>
2025-03-06 21:52:25 -0500

    Adds function to count occurances of characters

--- main.py
+++ main.py
@@ -1,11 +1,19 @@
 from stats import count_words
+from stats import count_characters
 
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     #print(text)
+    
     num_words = count_words(text)
     print(f"{num_words} words found in the document")
+
+    characters = count_characters(text)
+    print(characters)
+    #for c in chars:
+    #    print(f"'{c}': {chars[c]}")
+
 
 
 def get_book_text(file_path):
--- stats.py
+++ stats.py
@@ -1,3 +1,15 @@
 def count_words(text):
     word_list = text.split()
     return len(word_list)
+
+
+def count_characters(text):
+    characters = {}
+    for c in text:
+        c = c.lower()
+        if c in characters:
+            characters[c] += 1
+        else:
+            characters[c] = 1
+
+    return characters
