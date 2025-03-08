0a0c2c40a7cb7206ec71c764c02d23ef9a6381fe
Mathew877 <mbrevans@gmail.com>
2025-03-07 21:55:35 -0500

    Changes code to use command line arguments

--- main.py
+++ main.py
@@ -1,8 +1,12 @@
 from stats import count_words, count_characters, sort_characters
-
+import sys
 
 def main():
-    book_path = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    
+    book_path = sys.argv[1]
     text = get_book_text(book_path)
     num_words = count_words(text)
     character_counts = count_characters(text)
